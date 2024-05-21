a639496470e350994ad619adb63569e60cbbf794
Richard Abishai <rickoshade1891@gmail.com>
2024-05-21 15:24:28 +0530

    better

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Richard Abishai',
+    author_email= 'rickoshade1891@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
